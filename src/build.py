68260427280f867e225ccfcb7ed2b9e7e942e1dc
mathildeanitathierry <mathildeanitathierry@gmail.com>
2026-08-27 18:57:39 +0200

    update source files

--- src/build.py
+++ src/build.py
@@ -1,4 +1,3 @@
-eng=open("src/engine.js").read()
-act=open("src/actuator.js").read()
-open('index.html','w').write(open("src/shell.html").read().replace('<!--ENGINE-->', eng + '\n<script>\n' + act + '\n</script>'))
-import os; print('built', round(os.path.getsize('index.html')/1024/1024,2),'MB')
+eng=open('engine.js').read()
+open('coprod-landing.html','w').write(open('shell.html').read().replace('<!--ENGINE-->', eng))
+import os; print('built', round(os.path.getsize('coprod-landing.html')/1024/1024,2),'MB')
